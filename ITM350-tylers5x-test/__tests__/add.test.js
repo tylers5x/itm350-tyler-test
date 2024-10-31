0925e90c1eed088d4696d5e0b07bcaaeb8373f97
tylers5x <168314066+tylers5x@users.noreply.github.com>
2024-10-31 09:29:04 -0600

    Add GitHub Action for Docker build and push

--- ITM350-tylers5x-test/__tests__/add.test.js
+++
@@ -1,5 +0,0 @@
-const add = (a, b) => a + b;
-
-test('adds 2 + 3 to equal 5', () => {
-  expect(add(2, 3)).toBe(5);
-});
--- ITM350-tylers5x-test/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    coverageThreshold: {
-      './src/*.js': {
-        lines: 80
-      }
-    }
-  }
-  
